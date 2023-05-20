680e746084610739d2aa359e574fab0e504d5101
jose <joseluiss1208@gmail.com>
2023-05-20 10:42:19 -0700

    agregue opcion de vender

--- index.php
+++ index.php
@@ -19,16 +19,46 @@
     <?php
     require_once("db.php");
 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $id = $_POST["id"];
+      $cantidad = $_POST["cantidad"];
+
+      // Obtener el stock actual del platillo
+      $sql = "SELECT stock FROM platillos WHERE id='$id'";
+      $result = $conn->query($sql);
+
+      if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        $stockActual = $row["stock"];
+
+        // Verificar si la cantidad a vender es válida
+        if ($cantidad > 0 && $cantidad <= $stockActual) {
+          $nuevoStock = $stockActual - $cantidad;
+
+          // Actualizar el stock del platillo en la base de datos
+          $updateSql = "UPDATE platillos SET stock='$nuevoStock' WHERE id='$id'";
+          if ($conn->query($updateSql) === TRUE) {
+            echo '<div class="alert alert-success">Cantidad vendida actualizada correctamente.</div>';
+          } else {
+            echo '<div class="alert alert-danger">Error al actualizar la cantidad vendida: ' . $conn->error . '</div>';
+          }
+        } else {
+          echo '<div class="alert alert-danger">Cantidad inválida.</div>';
+        }
+      } else {
+        echo '<div class="alert alert-danger">Error: No se encontró el platillo.</div>';
+      }
+    }
+
+    // Obtener la lista de platillos
     $sql = "SELECT * FROM platillos";
     $result = $conn->query($sql);
-
 
     if ($result->num_rows > 0) {
       echo '<div> 
       <a href="add.php" class="btn btn-success mb-3">Agregar Platillo</a>
       <a href="pantallaplatillos.php" class="btn btn-primary mb-3">Ver Platillos</a>
-      <a href="login.php" class="btn btn-success mb-3">Cerrar sesion</a>
-
+      <a href="login.php" class="btn btn-success mb-3">Cerrar sesión</a>
       </div>';
       echo '<table class="table">';
       echo '<thead class="thead-dark">';
@@ -38,6 +68,7 @@
       echo '<th>Descripción</th>';
       echo '<th>Precio</th>';
       echo '<th>Imagen</th>';
+      echo '<th>Stock</th>';
       echo '<th>Acciones</th>';
       echo '</tr>';
       echo '</thead>';
@@ -49,7 +80,13 @@
         echo '<td>' . $row["descripcion"] . '</td>';
         echo '<td>$' . $row["precio"] . '</td>';
         echo '<td><img src="./imagenes/' . $row["imagen"] . '" width="100"></td>';
+        echo '<td>' . $row["Stock"] . '</td>';
         echo '<td>';
+        echo '<form method="post" action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '">';
+        echo '<input type="hidden" name="id" value="' . $row["id"] . '">';
+        echo '<input type="number" name="cantidad" min="1" max="' . $row["Stock"] . '" required>';
+        echo '<input type="submit" class="btn btn-success btn-sm" value="Vender">';
+        echo '</form>';
         echo '<a href="edit.php?id=' . $row["id"] . '" class="btn btn-primary btn-sm">Editar</a>';
         echo ' <a href="delete.php?id=' . $row["id"] . '" class="btn btn-danger btn-sm">Eliminar</a>';
         echo '</td>';
@@ -61,7 +98,6 @@
       echo '<div> 
       <a href="add.php" class="btn btn-success mb-3">Agregar Platillo</a>
       <a href="pantallaplatillos.php" class="btn btn-primary mb-3">Ver Platillos</a>
-      
       </div>';
       echo '<div class="alert alert-info">No hay platillos en la base de datos.</div>';
     }
